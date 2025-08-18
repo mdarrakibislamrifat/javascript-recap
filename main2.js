8236b67bce1cfc8b56ce98dd561434df22b948e2
mdarrakibislamrifat <rohanrifat43@gmail.com>
2025-08-18 16:02:26 +0600

    Copy to clipboard

---
+++ main2.js
@@ -0,0 +1,17 @@
+// 2. Copy to clipboard
+
+function copyToClipBoard(str) {
+    const element = document.createElement("textarea");
+    element.value = str;
+    document.body.appendChild(element);
+    element.select();
+    document.execCommand("copy");
+    document.body.removeChild(element);
+
+
+}
+
+function handleClick() {
+    let text = document.querySelector("#text");
+    copyToClipBoard(text.innerText);
+}
