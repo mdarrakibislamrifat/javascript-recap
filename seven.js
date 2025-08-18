5d54d04fed4b2a18ce83c20dc01a7cf6dfd65da3
mdarrakibislamrifat <rohanrifat43@gmail.com>
2025-08-18 12:40:28 +0600

    Log values with variable names smartly

---
+++ seven.js
@@ -0,0 +1,12 @@
+//  Log values with variable names smartly
+
+const library1 = "jQuery";
+const library2 = "React";
+const library3 = "Vue";
+
+
+console.log(`library1- ${library1}`);
+console.log(`library2- ${library2}`);
+console.log(`library3- ${library3}`);
+
+console.log({library1});
