af6966aa8e224302c7ec83f7c72ad7a6c56f89a5
mdarrakibislamrifat <rohanrifat43@gmail.com>
2025-08-18 12:17:00 +0600

    How to flatten a multi-dimensional array

--- four.js
+++ four.js
@@ -1,5 +1,5 @@
 //  How to flatten a multi-dimensional array
 
-let multiDimensionalArray = [[1, 2], [3, 4], [5, 6]];
+let multiDimensionalArray = [[1, 2], [3, 4], [5, 6],[7,8]];
 
 console.log(multiDimensionalArray.flat());
