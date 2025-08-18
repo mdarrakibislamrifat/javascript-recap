a6e3264d08d7ebb063f263274e0442d196768a2f
mdarrakibislamrifat <rohanrifat43@gmail.com>
2025-08-18 12:21:18 +0600

    Short condtionals

--- five.js
+++ five.js
@@ -1,1 +1,10 @@
-// 
+// Short condtionals
+
+const captain = "Mashrafi";
+
+if(captain === "Mashrafi") {
+    console.log("This is the captain");
+}
+
+
+captain === "Mashrafi" && console.log("This is the captain");
