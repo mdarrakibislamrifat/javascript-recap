e1963c20c0727be4d03bcaf6ffed988364c49230
mdarrakibislamrifat <rohanrifat43@gmail.com>
2025-08-18 12:28:48 +0600

    Replace all occurances of a string

---
+++ six.js
@@ -0,0 +1,6 @@
+// Replace all occurances of a string
+
+const quote = "The quick brown fox jumps over the lazy dog. The quick brown fox is very quick.";
+
+
+console.log(quote.replace(/quick/g, "slow"));
