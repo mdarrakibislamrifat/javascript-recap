2b0cd7e5ac73cfaf554951b5ae24ba0f8e9e70d9
mdarrakibislamrifat <rohanrifat43@gmail.com>
2025-08-18 12:16:20 +0600

    Resizing any array to a specific length

---
+++ four.js
@@ -0,0 +1,5 @@
+//  How to flatten a multi-dimensional array
+
+let multiDimensionalArray = [[1, 2], [3, 4], [5, 6]];
+
+console.log(multiDimensionalArray.flat());
---
+++ three.js
@@ -0,0 +1,5 @@
+//  Resizing any array to a specific length
+
+let animals = ['cat', 'dog', 'elephant', 'giraffe', 'lion'];
+animals.length = 3; 
+console.log(animals);
